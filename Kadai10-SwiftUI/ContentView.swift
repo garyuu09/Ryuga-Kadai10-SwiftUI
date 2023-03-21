7e9f2eac126be01ddb050be851a68f35e18a129b
Ryuga <ribereo.minami@gmail.com>
2023-03-21 23:03:49 +0900

    listの実装

--- Kadai10-SwiftUI/ContentView.swift
+++ Kadai10-SwiftUI/ContentView.swift
@@ -8,16 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    let prefecture = [
+        "北海道",
+        "青森県",
+        "岩手県",
+        "宮城県",
+        "秋田県",
+        "山形県",
+        "福島県",
+        "茨城県",
+        "栃木県",
+        "群馬県",
+        "埼玉県",
+        "千葉県",
+    ]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List {
+            VStack {
+                    ForEach (0..<12){ index in
+                        HStack {
+                            Text(prefecture[index])
+                            Spacer()
+                            Text(String(index + 1) + "番目の都道府県です")
+                        }
+                }
+            }
+            HStack {
+                Text("北海道")
+                Spacer()
+                Text("1番目の都道府県です")
+            }
+            .listRowBackground(Color.red)
+
+            HStack {
+                Text("青森県")
+                Spacer()
+                Text("2番目の都道府県です")
+            }
+            .listRowBackground(Color.green)
+
+            HStack {
+                Text("岩手県")
+                Spacer()
+                Text("3番目の都道府県です")
+            }
+            .listRowBackground(Color.blue)
         }
-        .padding()
+        .listStyle(.plain)
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
